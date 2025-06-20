1dcaebb68665efbeb520c6a8f3fc670c85db8166
rafammassa <rafaella_massa@hotmail.com>
2025-06-20 12:06:22 -0300

    Estilizando Component

---
+++ web-components/desafio-01/src/Components/TituloDinamico.js
@@ -0,0 +1,23 @@
+class TituloDinamico extends HTMLElement {
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: 'open' });
+
+    //base do componente
+    const componentRoot = document.createElement('h1');
+    componentRoot.textContent = 'Minha notícia';
+
+    //estilizar o componente
+    const style = document.createElement('style');
+    style.textContent = `
+      h1 {
+        color: #ff0000;
+      }
+    `;
+    //enviar para a shadow
+    shadow.appendChild(componentRoot);
+    shadow.appendChild(style);
+  }
+}
+
+customElements.define('titulo-dinamico', TituloDinamico);
